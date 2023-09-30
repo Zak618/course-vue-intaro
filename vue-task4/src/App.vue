d109856c8e24c280e7f5005a2490b8b8e9b65d63
Rolan <89405318+Zak618@users.noreply.github.com>
2023-09-30 19:34:52 +0300

    Uploaded the work

---
+++ vue-task4/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <Form @applyData="applyFormData" v-if="!dataApplied" />
+    <Resume :dataApplied="dataApplied" :photo="photo" :fio="fio" :birthday="birthday" :profession="profession"
+      :city="city" :phone="phone" :mail="mail" :education="education" :educationInstitution="educationInstitution"
+      :faculty="faculty" :specialization="specialization" :graduationYear="graduationYear" :salary="salary"
+      :skills="skills" :about="about" />
+  </div>
+</template>
+
+<script>
+import Form from './components/Form.vue';
+import Resume from './components/Resume.vue';
+
+export default {
+  components: {
+    Form,
+    Resume,
+  },
+  data() {
+    return {
+      dataApplied: false,
+      // Данные, которые будут переданы в Resume.vue
+      photo: '',
+      fio: '',
+      birthday: '',
+      profession: '',
+      city: '',
+      phone: '',
+      mail: '',
+      education: '',
+      educationInstitution: '',
+      faculty: '',
+      specialization: '',
+      graduationYear: '',
+      salary: '',
+      skills: '',
+      about: '',
+    };
+  },
+  methods: {
+    applyFormData(formData) {
+      // Обновление данных при событии "Применить" в Form.vue
+      this.dataApplied = true;
+      this.photo = formData.photo;
+      this.fio = formData.fio;
+      this.birthday = formData.birthday;
+      this.profession = formData.profession;
+      this.city = formData.city;
+      this.phone = formData.phone;
+      this.mail = formData.mail;
+      this.education = formData.education;
+      this.educationInstitution = formData.educationInstitution;
+      this.faculty = formData.faculty;
+      this.specialization = formData.specialization;
+      this.graduationYear = formData.graduationYear;
+      this.salary = formData.salary;
+      this.skills = formData.skills;
+      this.about = formData.about;
+    },
+  },
+};
+</script>
---
+++ vue-task4/src/components/Resume.vue
@@ -0,0 +1,115 @@
+<template>
+    <div class="right">
+      <div class="boxright mb-3 form-control" v-if="dataApplied">
+        <!-- ПОЛЯ ОТОБРАЖЕНИЯ -->
+        <div class="mb-3 fs-2">
+          <p>Ваше резюме: </p>
+        </div>
+  
+        <div class="mb-3">
+          <img :src="photo" class="photo img-fluid" alt="Ваша фотография">
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Ваше ФИО: </p>
+            <p class="fs-6">{{ fio }}</p>
+          </div>
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Ваша дата рождения: </p>
+            <p class="fs-6">{{ birthday }}</p>
+          </div>
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Ваша профессия: </p>
+            <p class="fs-6">{{ profession }}</p>
+          </div>
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Ваш город: </p>
+            <p class="fs-6">{{ city }}</p>
+          </div>
+        </div>
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Ваш номер телефона: </p>
+            <p class="fs-6">{{ phone }}</p>
+          </div>
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Ваша почта: </p>
+            <p class="fs-6">{{ mail }}</p>
+          </div>
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Ваше образование: </p>
+            <p class="fs-6">{{ education }}</p>
+          </div>
+        </div>
+  
+        <div v-if="education !== 'Среднее'">
+          <h2>Информация об образовании:</h2>
+          <p>Учебное заведение: {{ educationInstitution }}</p>
+          <p>Факультет: {{ faculty }}</p>
+          <p>Специализация: {{ specialization }}</p>
+          <p>Год окончания: {{ graduationYear }}</p>
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Желаемая зарплата: </p>
+            <p class="fs-6">{{ salary }}</p>
+          </div>
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">Ключевые навыки: </p>
+            <p class="fs-6">{{ skills }}</p>
+          </div>
+        </div>
+  
+        <div class="form-control">
+          <div class="mb-3 fs-5">
+            <p class="fs-5">О себе: </p>
+            <p class="fs-6">{{ about }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['dataApplied', 'photo', 'fio', 'birthday', 'profession', 'city', 'phone', 'mail', 'education', 'educationInstitution', 'faculty', 'specialization', 'graduationYear', 'salary', 'skills', 'about']
+  };
+  </script>
+  
+  <style scoped>
+  .right {
+    margin: 20px;
+    width: 50%;
+  }
+  
+  .photo {
+    width: 30%;
+    height: 30%;
+    border-radius: 50px;
+  }
+  
+  .form-control {
+    margin-top: 10px;
+  }
+  </style>
+  
